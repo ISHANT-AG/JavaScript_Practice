67d54a4d68364343a93fb9d966e8c4d735b02330
Ishant Agarwal <agarwalishant690@gmail.com>
2026-05-23 21:20:05 +0530

    ishant day-3

--- day3.js
+++ day3.js
@@ -1,3 +1,14 @@
+let person = {
+  name: "Ishant",
+  age: 19,
+  city: "Jaipur",
+  isStudent: true
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+console.log(person.isStudent);
 
 //2
 
