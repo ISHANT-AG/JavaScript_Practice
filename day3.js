e2c1bebc9d897ffb36ed60ebd703d9913649715c
Ishant Agarwal <agarwalishant690@gmail.com>
2026-05-26 21:31:06 +0530

    ishant day-6

--- day3.js
+++ day3.js
@@ -69,9 +69,3 @@ students.map(({ name, marks }) => {
 
 //10
 
-function sum(...nums) {
-    return nums.reduce(acc, n) => acc + n, 0);
-}
-
-console.log(sum(1, 2, 3));
-console.log(sum(10, 20, 30, 40));
